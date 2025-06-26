a35ab18d205408bd289783b4171579a15c6818f5
mridha-rakib <rakibmahmudmridha@gmail.com>
2025-06-26 23:17:06 +0600

    files modified

--- src/app/(main)/home/page.js
+++ src/app/(main)/home/page.js
@@ -14,49 +14,6 @@ import {
 } from "lucide-react";
 
 export default function Home() {
-  const stats = [
-    {
-      title: "CUSTOMERS",
-      value: "0",
-      icon: Users,
-      color: "border-l-blue-500",
-      bgColor: "bg-blue-50",
-      iconColor: "text-blue-500",
-    },
-    {
-      title: "EMPLOYEES",
-      value: "3",
-      icon: UserCheck,
-      color: "border-l-green-500",
-      bgColor: "bg-green-50",
-      iconColor: "text-green-500",
-    },
-    {
-      title: "PRODUCT",
-      value: "267",
-      icon: Package,
-      color: "border-l-cyan-500",
-      bgColor: "bg-cyan-50",
-      iconColor: "text-cyan-500",
-    },
-    {
-      title: "SUPPLIER",
-      value: "4",
-      icon: Users,
-      color: "border-l-orange-500",
-      bgColor: "bg-orange-50",
-      iconColor: "text-orange-500",
-    },
-    {
-      title: "OUT OF STOCKED PRODUCT",
-      value: "0",
-      icon: AlertTriangleIcon,
-      color: "border-l-red-500",
-      bgColor: "bg-red-50",
-      iconColor: "text-red-500",
-    },
-  ];
-
   return (
     <main className="flex flex-1 flex-col py-4 md:pt-3">
       <div className="flex items-center justify-between space-y-2 mb-6">
@@ -68,10 +25,6 @@ export default function Home() {
             Here&apos;s an overview for this IMS!
           </p>
         </div>
-        <Button>
-          <Plus />
-          Add Product
-        </Button>
       </div>
       <InvAnalytics />
       <div className="mt-4">
